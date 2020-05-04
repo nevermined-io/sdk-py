3e3edc242d5853823ae1c3da418ca0cf4c57bd10
eruizgar91 <enrique@keyko.io>
2020-05-04 18:03:14 +0200

    fix tests

--- tests/nevermined/test_agreements.py
+++ tests/nevermined/test_agreements.py
@@ -39,28 +39,115 @@ def agreements():
     )
 
 
-def test_prepare_agreement(agreements):
-    # consumer_account = get_consumer_account()
-    # ddo = get_ddo_sample()
-    # ocean_agreements.prepare(ddo.did, ServiceTypes.ASSET_ACCESS, consumer_account.address)
-    # :TODO:
-    pass
+def test_sign_agreement(publisher_instance, consumer_instance, registered_ddo):
+    # point consumer_instance's Gateway mock to the publisher's nevermined instance
+    Gateway.set_http_client(
+        GatewayMock(publisher_instance, publisher_instance.main_account))
 
+    consumer_acc = consumer_instance.main_account
+    keeper = Keeper.get_instance()
 
-def test_send_agreement(agreements):
-    pass
+    publisher_acc = publisher_instance.main_account
 
+    did = registered_ddo.did
+    asset_id = registered_ddo.asset_id
+    ddo = consumer_instance.assets.resolve(did)
+    service_agreement = ServiceAgreement.from_ddo(ServiceTypes.ASSET_ACCESS, ddo)
 
-def test_create_agreement(agreements):
-    pass
+    price = service_agreement.get_price()
 
+    # Give consumer some tokens
+    keeper.dispenser.request_vodkas(price * 2, consumer_acc)
 
-def test_agreement_release_reward():
-    pass
+    agreement_id, signature = consumer_instance.agreements.prepare(
+        did, consumer_acc, ServiceTypesIndices.DEFAULT_ACCESS_INDEX)
 
+    success = publisher_instance.agreements.create(
+        did,
+        ServiceTypesIndices.DEFAULT_ACCESS_INDEX,
+        agreement_id,
+        signature,
+        consumer_acc.address,
+        publisher_acc
+    )
+    assert success, 'createAgreement failed.'
 
-def test_agreement_refund_reward():
-    pass
+    event = keeper.escrow_access_secretstore_template.subscribe_agreement_created(
+        agreement_id,
+        10,
+        log_event(keeper.escrow_access_secretstore_template.AGREEMENT_CREATED_EVENT),
+        (),
+        wait=True
+    )
+    assert event, 'no event for AgreementCreated '
+
+    # Verify condition types (condition contracts)
+    agreement_values = keeper.agreement_manager.get_agreement(agreement_id)
+    assert agreement_values.did == asset_id, ''
+    cond_types = keeper.escrow_access_secretstore_template.get_condition_types()
+    for i, cond_id in enumerate(agreement_values.condition_ids):
+        cond = keeper.condition_manager.get_condition(cond_id)
+        assert cond.type_ref == cond_types[i]
+        assert int(cond.state) == 1
+
+    access_cond_id, lock_cond_id, escrow_cond_id = agreement_values.condition_ids
+    # Fulfill lock_reward_condition
+    starting_balance = keeper.token.get_token_balance(keeper.escrow_reward_condition.address)
+    keeper.token.token_approve(keeper.lock_reward_condition.address, price, consumer_acc)
+    tx_hash = keeper.lock_reward_condition.fulfill(
+        agreement_id, keeper.escrow_reward_condition.address, price, consumer_acc
+    )
+    keeper.lock_reward_condition.get_tx_receipt(tx_hash)
+    assert keeper.token.get_token_balance(
+        keeper.escrow_reward_condition.address) == (price + starting_balance), ''
+    assert keeper.condition_manager.get_condition_state(lock_cond_id) == 2, ''
+    event = keeper.lock_reward_condition.subscribe_condition_fulfilled(
+        agreement_id,
+        10,
+        log_event(keeper.lock_reward_condition.FULFILLED_EVENT),
+        (),
+        wait=True
+    )
+    assert event, 'no event for LockRewardCondition.Fulfilled'
+
+    # Fulfill access_secret_store_condition
+    tx_hash = keeper.access_secret_store_condition.fulfill(
+        agreement_id, asset_id, consumer_acc.address, publisher_acc
+    )
+    keeper.access_secret_store_condition.get_tx_receipt(tx_hash)
+    assert 2 == keeper.condition_manager.get_condition_state(access_cond_id), ''
+    event = keeper.access_secret_store_condition.subscribe_condition_fulfilled(
+        agreement_id,
+        10,
+        log_event(keeper.access_secret_store_condition.FULFILLED_EVENT),
+        (),
+        wait=True
+    )
+    assert event, 'no event for AccessSecretStoreCondition.Fulfilled'
+
+    # Fulfill escrow_reward_condition
+    tx_hash = keeper.escrow_reward_condition.fulfill(
+        agreement_id, price, publisher_acc.address,
+        consumer_acc.address, lock_cond_id,
+        access_cond_id, publisher_acc
+    )
+    keeper.escrow_reward_condition.get_tx_receipt(tx_hash)
+    assert 2 == keeper.condition_manager.get_condition_state(escrow_cond_id), ''
+    event = keeper.escrow_reward_condition.subscribe_condition_fulfilled(
+        agreement_id,
+        10,
+        log_event(keeper.escrow_reward_condition.FULFILLED_EVENT),
+        (),
+        wait=True
+    )
+    assert event, 'no event for EscrowReward.Fulfilled'
+    publisher_instance.assets.retire(did)
+
+    # path = consumer_instance.assets.consume(
+    #     agreement_id, did, service_definition_id,
+    #     consumer_acc, ConfigProvider.get_config().downloads_path
+    # )
+    # print('All good, files are here: %s' % path)
 
 
 def test_agreement_direct(publisher_instance, consumer_instance, metadata):
@@ -181,114 +268,3 @@ def test_agreement_status(setup_agreements_enviroment, agreements):
                                                               "escrowReward": 2
                                                               }
                                                }
-
-
-def test_sign_agreement(publisher_instance, consumer_instance, registered_ddo):
-    # point consumer_instance's Gateway mock to the publisher's nevermined instance
-    Gateway.set_http_client(
-        GatewayMock(publisher_instance, publisher_instance.main_account))
-
-    consumer_acc = consumer_instance.main_account
-    keeper = Keeper.get_instance()
-
-    publisher_acc = publisher_instance.main_account
-
-    did = registered_ddo.did
-    asset_id = registered_ddo.asset_id
-    ddo = consumer_instance.assets.resolve(did)
-    service_agreement = ServiceAgreement.from_ddo(ServiceTypes.ASSET_ACCESS, ddo)
-
-    price = service_agreement.get_price()
-
-    # Give consumer some tokens
-    keeper.dispenser.request_vodkas(price * 2, consumer_acc)
-
-    agreement_id, signature = consumer_instance.agreements.prepare(
-        did, consumer_acc, ServiceTypesIndices.DEFAULT_ACCESS_INDEX)
-
-    success = publisher_instance.agreements.create(
-        did,
-        ServiceTypesIndices.DEFAULT_ACCESS_INDEX,
-        agreement_id,
-        signature,
-        consumer_acc.address,
-        publisher_acc
-    )
-    assert success, 'createAgreement failed.'
-
-    event = keeper.escrow_access_secretstore_template.subscribe_agreement_created(
-        agreement_id,
-        10,
-        log_event(keeper.escrow_access_secretstore_template.AGREEMENT_CREATED_EVENT),
-        (),
-        wait=True
-    )
-    assert event, 'no event for AgreementCreated '
-
-    # Verify condition types (condition contracts)
-    agreement_values = keeper.agreement_manager.get_agreement(agreement_id)
-    assert agreement_values.did == asset_id, ''
-    cond_types = keeper.escrow_access_secretstore_template.get_condition_types()
-    for i, cond_id in enumerate(agreement_values.condition_ids):
-        cond = keeper.condition_manager.get_condition(cond_id)
-        assert cond.type_ref == cond_types[i]
-        assert int(cond.state) == 1
-
-    access_cond_id, lock_cond_id, escrow_cond_id = agreement_values.condition_ids
-    # Fulfill lock_reward_condition
-    starting_balance = keeper.token.get_token_balance(keeper.escrow_reward_condition.address)
-    keeper.token.token_approve(keeper.lock_reward_condition.address, price, consumer_acc)
-    tx_hash = keeper.lock_reward_condition.fulfill(
-        agreement_id, keeper.escrow_reward_condition.address, price, consumer_acc
-    )
-    keeper.lock_reward_condition.get_tx_receipt(tx_hash)
-    assert keeper.token.get_token_balance(
-        keeper.escrow_reward_condition.address) == (price + starting_balance), ''
-    assert keeper.condition_manager.get_condition_state(lock_cond_id) == 2, ''
-    event = keeper.lock_reward_condition.subscribe_condition_fulfilled(
-        agreement_id,
-        10,
-        log_event(keeper.lock_reward_condition.FULFILLED_EVENT),
-        (),
-        wait=True
-    )
-    assert event, 'no event for LockRewardCondition.Fulfilled'
-
-    # Fulfill access_secret_store_condition
-    tx_hash = keeper.access_secret_store_condition.fulfill(
-        agreement_id, asset_id, consumer_acc.address, publisher_acc
-    )
-    keeper.access_secret_store_condition.get_tx_receipt(tx_hash)
-    assert 2 == keeper.condition_manager.get_condition_state(access_cond_id), ''
-    event = keeper.access_secret_store_condition.subscribe_condition_fulfilled(
-        agreement_id,
-        10,
-        log_event(keeper.access_secret_store_condition.FULFILLED_EVENT),
-        (),
-        wait=True
-    )
-    assert event, 'no event for AccessSecretStoreCondition.Fulfilled'
-
-    # Fulfill escrow_reward_condition
-    tx_hash = keeper.escrow_reward_condition.fulfill(
-        agreement_id, price, publisher_acc.address,
-        consumer_acc.address, lock_cond_id,
-        access_cond_id, publisher_acc
-    )
-    keeper.escrow_reward_condition.get_tx_receipt(tx_hash)
-    assert 2 == keeper.condition_manager.get_condition_state(escrow_cond_id), ''
-    event = keeper.escrow_reward_condition.subscribe_condition_fulfilled(
-        agreement_id,
-        10,
-        log_event(keeper.escrow_reward_condition.FULFILLED_EVENT),
-        (),
-        wait=True
-    )
-    assert event, 'no event for EscrowReward.Fulfilled'
-    publisher_instance.assets.retire(did)
-
-    # path = consumer_instance.assets.consume(
-    #     agreement_id, did, service_definition_id,
-    #     consumer_acc, ConfigProvider.get_config().downloads_path
-    # )
-    # print('All good, files are here: %s' % path)
