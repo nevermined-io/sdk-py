7307bfce3e9895d5fd78188e2b16322dedb6a32c
Rodolphe Marques <marques.rodolphe@gmail.com>
2020-11-06 10:45:39 +0100

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/sdk-py',
-    version='0.5.0',
+    version='0.6.0',
     zip_safe=False,
 )
