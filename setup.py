07d7778fb74224167d5eb39c59e59ab7ff6c434a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-09-21 15:34:34 +0200

    Bump version: 0.10.2 → 0.10.3

--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/sdk-py',
-    version='0.10.2',
+    version='0.10.3',
     zip_safe=False,
 )
