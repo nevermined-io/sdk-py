083f2439390fb773c21585bd630cc5b4702988ea
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-09-22 09:31:06 +0200

    Bump version: 0.10.3 → 0.10.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = 'nevermined-sdk-py contributors'
 author = 'nevermined-sdk-py contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.10.3'
+release = '0.10.4'
 # The short X.Y version
 release_parts = release.split('.')  # a list
 version = release_parts[0] + '.' + release_parts[1]
--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/sdk-py',
-    version='0.10.3',
+    version='0.10.4',
     zip_safe=False,
 )
