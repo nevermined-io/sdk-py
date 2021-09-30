900b2cf8cd7f33ed3daead72aea6d8193de69a27
enrique <enrique@keyko.io>
2021-09-30 09:52:31 +0200

    Bump version: 0.10.4 → 0.10.5

--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/sdk-py',
-    version='0.10.4',
+    version='0.10.5',
     zip_safe=False,
 )
