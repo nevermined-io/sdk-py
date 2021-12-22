9fe5e4d08bc6b9d3ca4e03c52ed67851263cfa2f
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-12-22 12:27:29 +0100

    bumped version to 0.11.0

--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/sdk-py',
-    version='0.10.8',
+    version='0.11.0',
     zip_safe=False,
 )
